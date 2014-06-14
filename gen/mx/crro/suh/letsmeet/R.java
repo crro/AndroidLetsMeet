6f9034b3d4e55b82a0b11535f040b58388d96cbe
David Correa <David@Davids-MacBook-Pro-3.local>
2014-06-14 02:04:10 -0400

    Adding initial view

--- gen/mx/crro/suh/letsmeet/R.java
+++ gen/mx/crro/suh/letsmeet/R.java
@@ -8,7 +8,29 @@
 package mx.crro.suh.letsmeet;
 
 public final class R {
+    public static final class array {
+        public static final int time_array=0x7f080000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int caldroid_black=0x7f040001;
+        public static final int caldroid_darker_gray=0x7f040005;
+        public static final int caldroid_gray=0x7f040004;
+        /**  A dark Holo shade of blue 
+         */
+        public static final int caldroid_holo_blue_dark=0x7f040007;
+        /**  A light Holo shade of blue 
+         */
+        public static final int caldroid_holo_blue_light=0x7f040006;
+        /**  For settings framework 
+         */
+        public static final int caldroid_lighter_gray=0x7f040003;
+        /**  A really bright Holo shade of blue 
+         */
+        public static final int caldroid_sky_blue=0x7f040008;
+        public static final int caldroid_transparent=0x7f040002;
+        public static final int caldroid_white=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,25 +39,56 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f070000;
+        public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int calendar_next_arrow=0x7f020000;
+        public static final int calendar_prev_arrow=0x7f020001;
+        public static final int cell_bg=0x7f020002;
+        public static final int disable_cell=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int left_arrow=0x7f020005;
+        public static final int red_border=0x7f020006;
+        public static final int red_border_gray_bg=0x7f020007;
+        public static final int right_arrow=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f0a0011;
+        public static final int calendar1=0x7f0a0001;
+        public static final int calendar_gridview=0x7f0a0010;
+        public static final int calendar_left_arrow=0x7f0a000a;
+        public static final int calendar_month_year_textview=0x7f0a000b;
+        public static final int calendar_right_arrow=0x7f0a000c;
+        public static final int calendar_title_view=0x7f0a0009;
+        public static final int calendar_tv=0x7f0a000f;
+        public static final int endBtn=0x7f0a0004;
+        public static final int hourTxt=0x7f0a0007;
+        public static final int months_infinite_pager=0x7f0a000e;
+        public static final int scroller=0x7f0a0000;
+        public static final int startBtn=0x7f0a0003;
+        public static final int timeRange=0x7f0a0002;
+        public static final int timeZoneBtn=0x7f0a0005;
+        public static final int time_spinner=0x7f0a0006;
+        public static final int weekday_gridview=0x7f0a000d;
+        public static final int yolo=0x7f0a0008;
     }
     public static final class layout {
         public static final int activity_schedule=0x7f030000;
+        public static final int calendar_view=0x7f030001;
+        public static final int date_cell=0x7f030002;
+        public static final int date_grid_fragment=0x7f030003;
     }
     public static final class menu {
-        public static final int schedule=0x7f070000;
+        public static final int schedule=0x7f090000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int customize=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int show_dialog=0x7f050004;
+        public static final int undo=0x7f050005;
     }
     public static final class style {
         /** 
@@ -58,9 +111,31 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
--- src/mx/crro/suh/letsmeet/ScheduleActivity.java
+++ src/mx/crro/suh/letsmeet/ScheduleActivity.java
@@ -1,16 +1,150 @@
 package mx.crro.suh.letsmeet;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.text.format.DateFormat;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class ScheduleActivity extends Activity {
+import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
 
+@SuppressLint("ValidFragment")
+public class ScheduleActivity extends FragmentActivity implements OnItemSelectedListener {
+	private ArrayList<Date> _selected;
+	private Button _startBtn;
+	private Button _endBtn;
+	private boolean _startBool;
+	private boolean _endBool;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_schedule);
+		//Creating the formatter
+		final SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy", Locale.US);
+		
+		_selected = new ArrayList<Date>();
+		_startBtn = (Button) findViewById(R.id.startBtn);
+		_endBtn = (Button) findViewById(R.id.endBtn);
+		Button timeZoneBtn = (Button) findViewById(R.id.timeZoneBtn);
+		timeZoneBtn.setText("Time Zone: "+ TimeZone.getDefault().getDisplayName(false, TimeZone.SHORT));
+		//Used to determine what button to change
+		_startBool = false;
+		_endBool = false;
+		
+		//setting the listeners for the buttons
+		_startBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				_startBool = true;
+				_endBool = false;
+				DialogFragment newFragment = new TimePickerFragment();
+			    newFragment.show(getSupportFragmentManager(), "timePicker");
+			}
+		});
+		
+		_endBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				_startBool = false;
+				_endBool = true;
+				DialogFragment newFragment = new TimePickerFragment();
+			    newFragment.show(getSupportFragmentManager(), "timePicker");
+			}
+		});
+		
+		//Setting up the spinner
+		Spinner spinner = (Spinner) findViewById(R.id.time_spinner);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.time_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(this);
+		
+		//Setting up the fragment
+		CaldroidFragment caldroidFragment = new CaldroidFragment();
+		Bundle args = new Bundle();
+		Calendar cal = Calendar.getInstance();
+		args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+		args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+		caldroidFragment.setArguments(args);
+
+		FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+		t.replace(R.id.calendar1, caldroidFragment);
+		t.commit();
+		
+		//The listener of the Caldroid
+		final CaldroidListener listener = new CaldroidListener() {
+
+			@Override
+		    public void onSelectDate(Date date, View view) {
+		        Toast.makeText(getApplicationContext(), formatter.format(date),
+		                Toast.LENGTH_SHORT).show();
+		        //TODO: Stop the user form selecting dates on a wrong month.
+		        //TODO: Make it stay even if the user changes month.
+		        if (!_selected.contains(date)) {
+		        	view.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));
+		        	_selected.add(date);
+		        } else {
+		        	view.setBackgroundColor(getResources().getColor(android.R.color.white));
+		        	_selected.remove(date);
+		        }
+		    }
+
+		    @Override
+		    public void onChangeMonth(int month, int year) {
+		        String text = "month: " + month + " year: " + year;
+		        Toast.makeText(getApplicationContext(), text,
+		                Toast.LENGTH_SHORT).show();
+		    }
+
+		    @Override
+		    public void onLongClickDate(Date date, View view) {
+		        Toast.makeText(getApplicationContext(),
+		                "Long click " + formatter.format(date),
+		                Toast.LENGTH_SHORT).show();
+		    }
+
+		    @Override
+		    public void onCaldroidViewCreated() {
+		        Toast.makeText(getApplicationContext(),
+		                "Caldroid view is created",
+		                Toast.LENGTH_SHORT).show();
+		    }
+
+		};
+		caldroidFragment.setCaldroidListener(listener);
 	}
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -18,5 +152,58 @@ public class ScheduleActivity extends Activity {
 		getMenuInflater().inflate(R.menu.schedule, menu);
 		return true;
 	}
+	//Dialog class used for the UI pickers needed in this Activity.
+	public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
 
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current time as the default values for the picker
+			final Calendar c = Calendar.getInstance();
+			int hour = c.get(Calendar.HOUR_OF_DAY);
+			int minute = c.get(Calendar.MINUTE);
+
+			// Create a new instance of TimePickerDialog and return it
+			return new TimePickerDialog(getActivity(), this, hour, minute,
+					DateFormat.is24HourFormat(getActivity()));
+		}
+
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			// Do something with the time chosen by the user
+			if (ScheduleActivity.this._startBool) {
+				if (minute > 30) {
+					if (hourOfDay == 24) {
+						ScheduleActivity.this._startBtn.setText("Start\n " + 00 + ":" + "00");
+					} else {
+						ScheduleActivity.this._startBtn.setText("Start\n " + hourOfDay + 1+ ":" + "00");
+					}
+				} else {
+					ScheduleActivity.this._startBtn.setText("Start\n " + hourOfDay+ ":" + "00");
+				}
+			} else {
+				if (minute > 30) {
+					if (hourOfDay == 24) {
+						ScheduleActivity.this._endBtn.setText("Start\n " + "00" + ":" + "00");
+					} else {
+						ScheduleActivity.this._endBtn.setText("Start\n " + hourOfDay + 1 + ":" + "00");
+					}
+				} else {
+					ScheduleActivity.this._endBtn.setText("End\n " + hourOfDay+ ":" + "00");
+				}
+				
+			}
+		}
+	}
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
